cac22f6ceb9300279a094d520b5575ec78f92df2
mobioticsshashi <shashikumar.ravi@mobiotics.com>
2021-11-02 10:46:54 +0530

    FInal submission

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const items = ['en-US', 'en-GB', 'pt-BR'];
+
+const App = () => {
+  const [optionState, setOptionState] = useState(items[0]);
+
+  const date = new Date().toLocaleTimeString(optionState);
+
+  const handleOptionState = (e) => {
+    console.log(e.target.value);
+    setOptionState(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Language Time zone Assingment </h1>
+      <h3>Please select the Language</h3>
+      <select value={optionState} onChange={handleOptionState}>
+        {items.map((item, index) => (
+          <option value={item} key={index}>
+            {item}
+          </option>
+        ))}
+      </select>
+
+      <div className='result'>
+        <span className='normal'>Current Time in </span>{' '}
+        <span className='bold'> {optionState}</span>
+        <span className='normal'> language timezone is : </span>{' '}
+        <span className='bold'>{date} </span>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
